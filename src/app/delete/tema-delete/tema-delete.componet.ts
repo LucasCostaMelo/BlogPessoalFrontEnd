3a02033b9d559019af1c2e1d429004702be387b1
Lucas Melo <lucas.melo195@gmail.com>
2022-01-27 19:01:01 -0300

    Implementação Pute e Delete

---
+++ src/app/delete/tema-delete/tema-delete.componet.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from "@angular/router";
+import { environment } from "src/environments/environment.prod";
+
+
+@Component({
+    selector: 'app-tema-delete',
+    templateUrl: './tema-delete.component.html',
+    styleUrls: ['./tema-delete.component.css']
+})
+export class TemaDeleteComponent implements OnInit {
+
+    tema: Tema = new Tema()
+       idTema: number
+
+    constructor(
+        private temaService: TemaService,
+        private router: Router,
+        private route: ActivatedRoute
+    ) { }
+
+
+    ngOnInit() {
+        if(environment.token == ''){
+            this.router.navigate(['/entrar'])
+        }  
+
+
+     this.idTema = this.router.snapshot.params['id']
+     this.findByIdTema(this.idTema)
+    }
+
+    findByIdTema(id: number){
+        this.temaService.getByIdTema(id).subscribe((resp: Tema) =>{
+            this.tema = resp
+        } )
+    }
+
+
+    apagar(){
+        this.temaService.delteTema(this.idTema).subscribe(()=>{
+          alert('Tema  apagado com sucesso!') 
+          this.router.navigate(['/tema'])
+        })
+    }
+}
